e9fab45b7ceb375871f4dd5c76670ed6009fea48
Drashti Thakkar <drashtithakkar@Drashtis-MacBook-Pro.local>
2018-10-26 17:39:24 -0700

    adding new assignments

---
+++ Stalker/AgeCalculator.js
@@ -0,0 +1,6 @@
+var age=prompt("Enter your age");
+var current=now.getYear();
+var birthYear=age-current;
+var leap=
+var days=age*365;
+alert("You have lived for "+days+" number of days")
---
+++ Stalker/FirstJS.js
@@ -0,0 +1,5 @@
+var userFName = prompt("Enter your first name");
+var	userLName = prompt("Enter your last name");
+var age = prompt("Enter your age");
+console.log("Your full name is " +userFName+ " "+userLName);
+console.log("Your age is "+age);
